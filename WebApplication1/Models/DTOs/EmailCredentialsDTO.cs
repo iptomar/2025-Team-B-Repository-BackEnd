ebe07d26350b12012e3e26cb1abc49f271a34af8
Afonso Costa <121893968+afonsinhogamerpt@users.noreply.github.com>
2025-04-11 11:25:26 +0100

    serviço SMTP feito

---
+++ WebApplication1/Models/DTOs/EmailCredentialsDTO.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1.Models.DTOs;
+
+public class EmailCredentialsDTO
+{
+   public string body { get; set; } 
+   public string to { get; set; } 
+   public string subject { get; set; } 
+   
+}
--- WebApplication1/Models/DTOs/SalaDTO.cs
+++ WebApplication1/Models/DTOs/SalaDTO.cs
@@ -3,6 +3,6 @@ namespace WebApplication1.Models.DTOs;
 public class SalaDTO
 {
     public int Id_sala { get; set; }
-    public string localidade { get; set; } //em vez de ter isto, ter realmente o nome de cada localidade 
+    public string localidade { get; set; } 
     public string Nome_sala { get; set; }
 }
