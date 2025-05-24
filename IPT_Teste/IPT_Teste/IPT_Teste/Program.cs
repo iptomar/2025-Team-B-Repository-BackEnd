9ccaa70dfcb173b628fad33fb8a70bbd24cbaa56
Afonso Costa <121893968+afonsinhogamerpt@users.noreply.github.com>
2025-05-24 15:23:09 +0100

    update Program.cs

--- IPT_Teste/IPT_Teste/IPT_Teste/Program.cs
+++ IPT_Teste/IPT_Teste/IPT_Teste/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using IPT_Teste.Data;
+using WebApplication1.Controllers.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +27,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
+builder.Services.AddSignalR();
 
 var jwtKey = builder.Configuration["Jwt:Key"] ?? Environment.GetEnvironmentVariable("JWT_KEY");
 
@@ -60,7 +62,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
-
+app.MapHub<BlocoHubController>("hubdobloco");
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
