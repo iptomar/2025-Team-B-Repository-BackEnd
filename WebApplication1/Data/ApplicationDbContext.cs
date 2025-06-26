d4a720bfcd2f5d7d5a7d182463d5ec53a0ab35e8
bea04mm <beatrizmmoreira04@gmail.com>
2025-06-26 23:36:47 +0100

    Correção do merge.

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+using WebApplication1.Models.DTOs;
+
+namespace WebApplication1.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+    public DbSet<Curso> Curso { get; set; }
+
+    public DbSet<Cadeira> Cadeira { get; set; }
+
+    public DbSet<Grau> Grau { get; set; }
+
+    public DbSet<Instituicao> Instituicao { get; set; }
+    
+    public DbSet<Sala> Sala { get; set; }
+    
+    public DbSet<Localidade> Localidade { get; set; }
+}
---
+++ WebApplication1/Models/DTOs/LocalidadeDTO.cs
@@ -0,0 +1,7 @@
+namespace WebApplication1.Models.DTOs;
+
+public class LocalidadeDTO
+{
+    public int Id_localidade { get; set; }
+    public string Nome_localidade { get; set; }
+}
